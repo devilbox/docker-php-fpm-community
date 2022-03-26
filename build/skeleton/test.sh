6fa252262e91c60e63bd6d48fb4429054a3282f2
cytopia <cytopia@everythingcli.org>
2022-03-26 15:54:00 +0100

    Fix custom tests

--- build/skeleton/test.sh
+++ build/skeleton/test.sh
@@ -9,7 +9,6 @@ CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 IMAGE="${1}"
 ARCH="${2}"
 VERSION="${3}"
-FLAVOUR="work"
 TAG="${4}"
 
 
@@ -30,8 +29,8 @@ if [ -z "${TESTS}" ]; then
 fi
 for t in ${TESTS}; do
 	printf "\n\n\033[0;33m%s\033[0m\n" "################################################################################"
-	printf "\033[0;33m%s %s\033[0m\n"  "#" "[${VERSION}-${FLAVOUR}] (${ARCH})"
-	printf "\033[0;33m%s %s\033[0m\n"  "#" "${t} ${IMAGE} ${ARCH} ${VERSION} ${FLAVOUR} ${TAG}"
+	printf "\033[0;33m%s %s\033[0m\n"  "#" "[${VERSION}] (${ARCH})"
+	printf "\033[0;33m%s %s\033[0m\n"  "#" "${t} ${IMAGE} ${ARCH} ${VERSION} ${TAG}"
 	printf "\033[0;33m%s\033[0m\n\n"   "################################################################################"
-	time ${t} "${IMAGE}" "${ARCH}" "${VERSION}" "${FLAVOUR}" "${TAG}"
+	time ${t} "${IMAGE}" "${ARCH}" "${VERSION}" "${TAG}"
 done
